9a67776f9630841764b382510c86e37fae174873
aiden <danyang.aiden@outlook.com>
2022-12-04 03:39:38 +0800

    test updater

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import { checkUpdate, installUpdate } from "@tauri-apps/api/updater";
 import { relaunch } from "@tauri-apps/api/process";
+import { message } from '@tauri-apps/api/dialog'
+import { onMounted } from "vue";
 
-try {
-  const { shouldUpdate, manifest } = await checkUpdate();
-  if (shouldUpdate) {
-    // display dialog
-    await installUpdate();
-    // install complete, restart app
-    await relaunch();
+onMounted(async () => {
+  try {
+    const { shouldUpdate, manifest } = await checkUpdate();
+    if (shouldUpdate) {
+      // display dialog
+      await installUpdate();
+      // install complete, restart app
+      await relaunch();
+    }
+  } catch (error) {
+    console.log(error, 'aaa');
+    message(error as string)
   }
-} catch (error) {
-  console.log(error);
-}
+})
 
 </script>
 
