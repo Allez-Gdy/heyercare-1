a71820f049228dc68dd570b776e4d6952286f649
aiden <danyang.aiden@outlook.com>
2022-12-04 00:27:25 +0800

    sub unbuntu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
-
+// import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
+// import { relaunch } from '@tauri-apps/api/process'
+// try {
+//   const { shouldUpdate, manifest } = await checkUpdate()
+//   if (shouldUpdate) {
+//     // display dialog
+//     await installUpdate()
+//     // install complete, restart the app
+//     await relaunch()
+//   }
+// } catch (error) {
+//   console.log(error)
+// }
+import { listen } from '@tauri-apps/api/event'
+listen('tauri://update-available', function (res) {
+  console.log('aaa');
+  
+  console.log('New version available: ', res)
+})
 </script>
 
 <template>
