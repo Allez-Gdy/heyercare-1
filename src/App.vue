e1ca6a45063a953ad9d06907dd5b0726501680a5
aiden <danyang.aiden@outlook.com>
2022-12-04 02:25:10 +0800

    test uploader

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,30 @@
 // } catch (error) {
 //   console.log(error)
 // }
-import { listen } from '@tauri-apps/api/event'
-listen('tauri://update-available', function (res) {
-  console.log('aaa');
-  
-  console.log('New version available: ', res)
+import { ask, confirm } from '@tauri-apps/api/dialog' 
+import { onMounted } from 'vue';
+import { checkUpdate , installUpdate, onUpdaterEvent} from '@tauri-apps/api/updater';
+
+// confirm(
+//   'This action cannot be reverted. Are you sure?', { 
+//     title: 'Tauri', 
+//     type: 'warning' 
+//   }
+// ).then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// })
+onMounted(async () => {
+  const update = await checkUpdate();
+  if (update.shouldUpdate) {
+    console.log(`Installing update ${update.manifest?.version}, ${update.manifest?.date}, ${update.manifest!.body}`);
+    await installUpdate();
+  }
+  // const unlisten = await onUpdaterEvent(({ error, status }) => {
+  //   console.log('Updater event', error, status);
+  // });
+  // unlisten();
 })
 </script>
 
