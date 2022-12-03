1e1c10661ffbd4d1926fbaf2986fde747d72bbd6
aiden <danyang.aiden@outlook.com>
2022-12-04 02:43:54 +0800

    test updater3

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,19 @@
 <script setup lang="ts">
-// import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
-// import { relaunch } from '@tauri-apps/api/process'
-// try {
-//   const { shouldUpdate, manifest } = await checkUpdate()
-//   if (shouldUpdate) {
-//     // display dialog
-//     await installUpdate()
-//     // install complete, restart the app
-//     await relaunch()
-//   }
-// } catch (error) {
-//   console.log(error)
-// }
-import { ask, confirm } from '@tauri-apps/api/dialog' 
-import { onMounted } from 'vue';
-import { checkUpdate , installUpdate, onUpdaterEvent} from '@tauri-apps/api/updater';
+import { checkUpdate, installUpdate } from "@tauri-apps/api/updater";
+import { relaunch } from "@tauri-apps/api/process";
 
-// confirm(
-//   'This action cannot be reverted. Are you sure?', { 
-//     title: 'Tauri', 
-//     type: 'warning' 
-//   }
-// ).then(res => {
-//   console.log(res);
-// }).catch(err => {
-//   console.log(err);
-// })
-onMounted(async () => {
-  const update = await checkUpdate();
-  if (update.shouldUpdate) {
-    console.log(`Installing update ${update.manifest?.version}, ${update.manifest?.date}, ${update.manifest!.body}`);
+try {
+  const { shouldUpdate, manifest } = await checkUpdate();
+  if (shouldUpdate) {
+    // display dialog
     await installUpdate();
+    // install complete, restart app
+    await relaunch();
   }
-  // const unlisten = await onUpdaterEvent(({ error, status }) => {
-  //   console.log('Updater event', error, status);
-  // });
-  // unlisten();
-})
+} catch (error) {
+  console.log(error);
+}
+
 </script>
 
 <template>
