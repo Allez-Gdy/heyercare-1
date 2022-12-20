1e3c7b2d6c1deafcc74402ba640831ce4bbf9be0
aiden <danyang.aiden@outlook.com>
2022-12-20 22:52:54 +0800

    feat:readfile

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,11 +6,29 @@
 mod db;
 mod app;
 
+use std::{fs::File, io::{Read, BufReader, BufRead, Seek, SeekFrom}};
+
 use db::db_utils:: init_db ;
 use app:: patients ;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     init_db().expect("初始化数据库失败！");
+
+    let mut file = File::open("src/72710009.Bkp")?;
+    // let mut buffer = [0u8; 8];
+    // println!("{:?}", buffer);
+    // let data = file.read(&mut buffer);
+    // println!("{:?}", buffer);
+    file.seek(SeekFrom::Start(2))?;
+    let mut reader = BufReader::new(file);
+    let mut line = [0u8; 8];
+    // reader.read_line(&mut line);
+    reader.read_exact(&mut line);
+
+    println!("{:?}", reader);
+    println!("{:?}", line);
+
+    
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
@@ -18,4 +36,6 @@ fn main() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    Ok(())
 }
